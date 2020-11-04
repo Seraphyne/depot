444cbafca82d0bcff8975ef01db490196e69fdf4
Seraphyne <renatafd717@gmail.com>
2020-11-04 16:21:46 -0600

    connecting product to Carts

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,16 +1,26 @@
 class Product < ApplicationRecord
-    #validates method that checks if a field is mandatory
-    # presence true don't let those fields be empty
+
+    has_many :line_items
+    has_many :orders, through: :line_items
+  
+    before_destroy :ensure_not_referenced_by_any_lineitem
+  
     validates :title, :description, :image_url, presence: true
-    validates :price, numericality: {greater_than_or_equal_to: 0.01}
-    # price is valid and positive number bigger than 0.01
     validates :title, uniqueness: true
-    # it will check to ensure that on that table there's only one title with that name
+    validates :price, numericality: { greater_than_or_equal_to: 0.01 }
     validates :image_url, allow_blank: true, format: {
-        with: %r{\.(gif|jpg|png)\Z}i,
-        message: 'must be a URL for gif, jpg or png image.'
-    }
-    # checks for format and what is a valid submition for an image
-end
+                  with: %r{\.(gif|jpg|png)\Z}i,
+                  message: "must be a URL for GIF, JPG or PNG image.",
+                }
+  
+    private
+      # Ensure there are no line items referencing thi=s product (Edge case)
+      def ensure_not_referenced_by_any_line_item
+        unless line_items.empty?
+          errors.add(:base, 'Line Items present')
+          throw :abort
+        end
+      end
+  end
 
 # PAGE 87 forward.
