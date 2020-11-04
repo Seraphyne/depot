0ffec48fbcb02649c2ec10037303d985332dee1f
Seraphyne <renatafd717@gmail.com>
2020-11-04 11:49:37 -0600

    added testing

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,77 +1,62 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  fixtures :products # loads test data into the products table(customizable)
+  # Rails loads all fixtures b4 running any test
 
-  fixtures :products
+  test 'products attributes must not be empty' do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+  end
 
-    test "product attributes must not be empty" do
-      product = Product.new
-      assert product.invalid?
-      assert product.errors[:title].any?
-      assert product.errors[:description].any?
-      assert product.errors[:price].any?
-      assert product.errors[:image_url].any?
+  test 'product price must be positive' do
+    product = Product.new title: 'My Book Title', description: 'yyy', image_url: 'zzz.jpg'
+
+    product.price = -1
+    assert product.invalid?
+    assert_equal ['must be greater than or equal to 0.01'], product.errors[:price]
+
+    product.price = 0
+    assert product.invalid?
+    assert_equal ['must be greater than or equal to 0.01'], product.errors[:price]
+
+    product.price = 1
+    assert product.valid?
+  end
+
+  def new_product(image_url)
+    Product.new(
+      title: 'My Book Title',
+      description: 'yyy',
+      price: 1,
+      image_url: image_url
+    )
+  end
+
+  test 'image_url' do
+    ok = %w{fred.gif fred.png fred.jpg FRED.JPG FRED.Jpg http://a.b.c/x/y/z/fred.gif}
+    bad = %w{fred.doc fred.gif/more fred.gif.more}
+
+    ok.each do |image_url|
+      assert new_product(image_url).valid?, "#{image_url} shouldn't be invalid"
     end
 
-    test "product price must be positive" do
-        product = Product.new(
-          title: "My book title", 
-          description: "This is a new description",
-          image_url: "7apps.jpg"
-      )
-      product.price = -1
-      assert product.invalid?
-      assert_equal ["must be greater than or equal to 0.01"],
-        product.errors[:price]
-
-      product.price = 0
-      assert product.invalid?
-      assert_equal ["must be greater than or equal to 0.01"],
-        product.errors[:price]
-
-      product.price = 1
-      assert.product.valid?
+    bad.each do |image_url|
+      assert new_product(image_url).invalid?, "#{image_url} shouldn't be valid"
     end
+  end
 
-    def new_product(image_url)
-      Product.new(
-        title: "The One", 
-        description: "This is a book description", 
-        price: 1, 
-        image_url: image_url
-      )
-    end
-
-    test "image_url" do
-      ok = %W{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg 
-              http://a.b.c/x/y/z/fred.gif }
-
-      bad = %W{ fred.doc fred.gif/more fred.gif.more}
-      
-      ok.each do |image_url|
-        assert new_product(image_url).valid?,
-            "#{image_url} shouldn't be invalid."
-      end
-
-      bad.each do |image_url|
-        assert new_product(image_url).invalid?,
-            "#{image_url} shouldn't be valid."
-      end
-
-    end
-
-    test "product is not valid without a unique title - Il8n" do
-      product = Product.new(
-        title: products(:three).title,
-        description: "yyyy",
-        price: 1,
-        image_url: "7apps.jpg"
-      )
-
-      assert product.invalid?
-      assert_equal [I18n.translate('errors.messages.taken')],
-                    product.errors[:title] 
-      
-    end
-
+  test 'product not valid without unique title' do
+    product = Product.new(title: products(:ruby).title,
+                         description: 'yyy',
+                         price: 1,
+                         image_url: 'fred.gif')
+    assert product.invalid?
+    # assert_equal ['has already been taken'], product.errors[:title]
+    assert_equal [I18n.translate('errors.messages.taken')], product.errors[:title]
+  end
 end
