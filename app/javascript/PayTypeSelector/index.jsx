4923356fcadfae882ac33de8c97eb0eb1f15b05d
Seraphyne <renatafd717@gmail.com>
2020-11-06 13:10:59 -0600

    payment options in react completed

--- app/javascript/PayTypeSelector/index.jsx
+++ app/javascript/PayTypeSelector/index.jsx
@@ -1,23 +1,50 @@
 import React from 'react';
+import NoPayType from './NoPayType';
+import CreditCardPayType from './CreditCardPayType';
+import CheckPayType from './CheckPayType';
+import PurchaseOrderPayType from './PurchaseOrderPayType';
 
 export default class PayTypeSelector extends React.Component{
+    constructor(props){
+        super(props);
+        this.onPayTypeSelected = this.onPayTypeSelected.bind(this);
+        this.state = {
+            selectedPayType: null,
+        }
+    }
     onPayTypeSelected(event){
-        console.log(event.target.value);
+        this.setState({ selectedPayType: event.target.value});
     }
     render(){
+
+        let PayTypeCustomerComponent = NoPayType;
+
+        if(this.state.selectedPayType == 'Credit Card'){
+            PayTypeCustomerComponent = CreditCardPayType;
+        }
+        else if(this.state.selectedPayType == 'Check'){
+            PayTypeCustomerComponent = CheckPayType;
+        }
+        else if(this.state.selectedPayType == 'Purchase Order'){
+            PayTypeCustomerComponent = PurchaseOrderPayType;
+        }
+
         return(
-            <div className="field">
-                <label htmlFor='order_pay_type'>Pay Type</label>
-                    <select 
-                        id='order_pay_type'
-                        onChange={this.onPayTypeSelected} 
-                        name='order[pay_type]'
-                    >
-                        <option value=''>Select a payment method</option>
-                        <option value='Check'>Check</option>
-                        <option value='Credit Card'>Credit Card</option>
-                        <option value='Purchase Online'>Purchase Online</option>
-                    </select>
+            <div>
+                <div className="field">
+                    <label htmlFor='order_pay_type'>Pay Type</label>
+                        <select 
+                            id='order_pay_type'
+                            onChange={this.onPayTypeSelected} 
+                            name='order[pay_type]'
+                        >
+                            <option value=''>Select a payment method</option>
+                            <option value='Check'>Check</option>
+                            <option value='Credit Card'>Credit Card</option>
+                            <option value='Purchase Order'>Purchase Order</option>
+                        </select>
+                </div>
+                <PayTypeCustomerComponent />
             </div>
         );
     }
