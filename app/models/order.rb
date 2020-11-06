8c916b4f77f0d5f90c6b2666d5079354c9e6398e
Seraphyne <renatafd717@gmail.com>
2020-11-06 14:59:03 -0600

    fixed a typo that was giving the credit cart error

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,7 @@ class Order < ApplicationRecord
     has_many :line_items, dependent: :destroy
     enum pay_type: {
         "Check" => 0,
-        "Credit Cart" => 1,
+        "Credit Card" => 1,
         "Purchase Order" => 2
     }
 
