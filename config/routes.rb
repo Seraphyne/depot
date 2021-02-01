c5bfc448bec4286eee4f9c23550dafe8e0689a8f
RD <renatafd717@gmail.com>
2021-02-01 11:04:45 -0600

    selecting locale

--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,11 @@ Rails.application.routes.draw do
   resources :products do
     get :who_bought, on: :member
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'sore_index', via: :all
+  end
 end
